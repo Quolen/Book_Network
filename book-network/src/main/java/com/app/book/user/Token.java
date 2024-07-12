ac032e34f97331badcf19b3f267beaaacb347219
Quolen <oleg.silvika@gmail.com>
2024-07-12 17:33:53 +0200

    Implemented Token entity and Repositories.

---
+++ book-network/src/main/java/com/app/book/user/Token.java
@@ -0,0 +1,33 @@
+package com.app.book.user;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Token {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String token;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime expiresAt;
+    private LocalDateTime validatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "userId", nullable = false)
+    private User user;
+
+}
